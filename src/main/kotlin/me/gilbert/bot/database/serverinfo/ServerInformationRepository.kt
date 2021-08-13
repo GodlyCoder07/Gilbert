c704e783aec038fe34f3d0815811b581369ccb50
GodlyCoder07 <84781191+GodlyCoder07@users.noreply.github.com>
2021-08-13 15:27:30 +0800

    Update ServerInformationRepository.kt

--- src/main/kotlin/me/gilbert/bot/database/serverinfo/ServerInformationRepository.kt
+++ src/main/kotlin/me/gilbert/bot/database/serverinfo/ServerInformationRepository.kt
@@ -8,14 +8,6 @@ import java.io.FileReader
 import java.io.FileWriter
 
 class ServerInformationRepository(guildId: String, path: File): Data {
-    /*
-     *  Today I created @PixelCord. Right now, he does nothing. What I am planning to do is to connect Discord and Minecraft chat.
-     *  Every message you send in a particular channel will be received by the bot, @PixelCord and sends it to the Minecraft server
-     *  that has been assigned to it. I will also upload the plugin in spigot for others to use in their Minecraft server. Today I
-     *  created @PixelCord. Right now, he does nothing. What I am planning to do is to connect Discord and Minecraft chat. Every message
-     *  you send in a particular channel will be received by the bot, @PixelCord and sends it to the Minecraft server that has been
-     *  assigned to it. I will also upload the plugin in spigot for others to use in their Minecraft server.
-     */
     private val gson: Gson = GsonBuilder().serializeNulls().setPrettyPrinting().create()
     private lateinit var serverInformationModel: ServerInformationModel
     private val file = File(path, "data_server-info_$guildId.json")
